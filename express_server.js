b5ce5f2f5706e976516b66d700c4ba09bb4771ed
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-12 19:32:18 -0400

    generateRandom str fn and urls_new page

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {}
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,14 +21,23 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars)
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
