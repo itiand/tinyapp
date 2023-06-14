3bf98834663411a1220b5ff7dd2834fa8499ba1d
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-14 13:38:02 -0400

    create /register post route, adding new user to users database

--- express_server.js
+++ express_server.js
@@ -5,11 +5,26 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  itian: {
+    id: "itian",
+    email: "itian@example.com",
+    password: "password",
+  },
+  negativedelos: {
+    id: "negativedelos",
+    email: "itian@example.com",
+    password: "password1",
+  },
+}
 
 function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -21,8 +36,10 @@ function generateRandomString() {
   return randomString;
 }
 
-app.use(express.urlencoded({ extended: true }));
 
+///ROUTING/////
+////////////////
+///////////////
 app.get('/urls', (req, res) => {
   const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   // console.log(req.cookies);
@@ -47,7 +64,6 @@ app.post("/urls", (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  console.log('REQ BODY', req.body);
   res.cookie('username', req.body.username);
 
   res.redirect('/urls');
@@ -57,6 +73,21 @@ app.post('/logout', (req,res) => {
   res.clearCookie('username')
   res.redirect('/urls');
 })
+
+app.post('/register', (req,res) => {
+  // const newUser = {}
+  const id = generateRandomString()
+
+  //updates our 'users database, parameters from req.body and generatedID
+  users[id] = { id, email : req.body.email, password : req.body.password  }
+
+  //then set the cookie to the generated id --> user_id = genertatedID
+  res.cookie('user_id', users[id].id);
+
+  //redirect tp the urls page
+  res.redirect('/urls')
+})
+
 
 ///VARIABLE ROUTES
 app.get("/u/:id", (req, res) => {
