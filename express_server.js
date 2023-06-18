ace41f03cd39aa4b8b8b4eeeb278267cc8eb7502
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-17 23:42:18 -0400

    switch column position on /urls

--- express_server.js
+++ express_server.js
@@ -97,6 +97,7 @@ app.get("/login", (req, res) => {
   const currentUser = users[req.session.user_id];
   const templateVars = { userObj: currentUser };
   res.render('login', templateVars);
+  //return
 });
 
 app.post("/urls", (req, res) => {
